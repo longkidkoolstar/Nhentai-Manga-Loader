4191c59d30668d14c76840bc4db68740798c2cf6
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2024-10-05 16:49:08 -0400

    ver 4.3.4
    made it call every 100 milisecond
    has initial call
    comment out logger

--- NhentaiMangaLoader.user.js
+++ NhentaiMangaLoader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nhentai Manga Loader
 // @namespace    http://www.nhentai.net
-// @version      4.3.3.1
+// @version      4.3.4
 // @description  Loads nhentai manga chapters into one page in a long strip format with image scaling, click events, and a dark mode for reading.
 // @match        *://nhentai.net/g/*/*
 // @icon         https://clipground.com/images/nhentai-logo-5.png
@@ -1108,7 +1108,7 @@ async function saveCurrentPosition(mangaId) {
         const findSimilarButtons = document.querySelectorAll('.find-similar');
     
         if (findSimilarButtons.length > 0) {
-            console.log('Find Similar button already exists.');
+            //console.log('Find Similar button already exists.');
         } else if (!document.body.contains(loadMangaButton)) { 
             document.body.appendChild(loadMangaButton);
     
@@ -1141,9 +1141,9 @@ async function saveCurrentPosition(mangaId) {
             });
         }
     }
-    
+    checkForFindSimilarButton();
     // Set an interval to check every second (1000 milliseconds)
-    setInterval(checkForFindSimilarButton, 1000);
+    setInterval(checkForFindSimilarButton, 100);
     
         
     })();
